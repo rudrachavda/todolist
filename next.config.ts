fee1220e0f2f7af2319a341df236c12f7b9ba893
Rudra Chavda <rudrabchavda@gmail.com>
2026-07-11 14:21:59 -0700

    commit before migrating to supabase

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  allowedDevOrigins: ['192.168.7.110'],
+}
 
 export default nextConfig;
